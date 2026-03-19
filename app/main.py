ac535346a5aa5cd527177d5f176758ea2116888b
Nicolás Moya <nmoya@nubenc.com>
2026-03-19 17:45:00 -0300

    e

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-from app.routes import tickets # pyright: ignore[reportMissingImports]
-from app.database import engine, Base ,get_db # pyright: ignore[reportMissingImports]
+from fastapi.middleware.cors import CORSMiddleware
+from app.database import engine, Base
+from app.routes import tickets
+from app.routes import auth  # importá el router de auth
+
+app = FastAPI()  # ← esto faltaba antes del middleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-
-app.include_router(tickets.router)
+app.include_router(tickets.router, prefix="/tickets", tags=["tickets"])
+app.include_router(auth.router, prefix="/auth", tags=["auth"])  # ← registrar auth
